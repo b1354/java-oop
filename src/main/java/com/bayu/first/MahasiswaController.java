bb6cbf75c39e49b0eb60cb9c16b005a1203d1545
b1354 <bayurizkyseptiansyah@gmail.com>
2025-04-10 15:10:19 +0700

    menambahkan get parameter, dan delete

--- src/main/java/com/bayu/first/MahasiswaController.java
+++ src/main/java/com/bayu/first/MahasiswaController.java
@@ -9,10 +9,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -25,6 +29,21 @@ public class MahasiswaController {
         String sql = "SELECT * FROM mhs";
         return koneksi.query(sql, new RowMapperMhs());
     }
+
+    @GetMapping("ambilData/{id}")
+    public List<Mahasiswa> getMHsByNim(@PathVariable String id) {
+        String sql = "SELECT * FROM mhs WHERE c_nim = ?";
+        List<Mahasiswa> data = koneksi.query(sql, new RowMapperMhs(), id);
+        return data;
+    }
+    
+
+    @GetMapping("/ambilData")
+    public List<Mahasiswa> getMhsByParam(@RequestParam String nama, @RequestParam String alamat) {
+        String sql = "SELECT * FROM mhs WHERE n_nama LIKE '%" + nama + "%' AND n_alamat Like '%"+ alamat + "%'";
+        return koneksi.query(sql, new RowMapperMhs());
+    }
+    
 
     @PostMapping("/insert")
     public String inserMahasiswa(@RequestBody Mahasiswa data) {
@@ -50,6 +69,20 @@ public class MahasiswaController {
             hasil = "berhasil mengubah data";
         } catch (Exception e) {
             hasil = "tidak dapat mengubah data";
+            System.out.println(e);
+        }
+        return hasil;
+    }
+
+    @DeleteMapping("/delete/{nim}")
+    public String deleteMhs(@PathVariable String nim) {
+        String hasil;
+        String sql = "DELETE FROM mhs WHERE c_nim = ?";
+        try {
+            koneksi.update(sql, nim);
+            hasil = "berhasil menghapus data";
+        } catch (Exception e) {
+            hasil = "tidak dapat menghapus data";
             System.out.println(e);
         }
         return hasil;
